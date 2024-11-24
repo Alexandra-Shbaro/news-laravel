aca93cade5f5df1f1173b5a088348f6d3b1c65db
Alexandra Shbaro <alexandrashbaro@gmail.com>
2024-11-24 21:17:26 +0200

    added api to allow user to post article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\News;
 
 class ArticleController extends Controller
 {
-    //
+    //enforcing user authentication
+    public function __construct()
+    {
+        $this->middleware('auth:api')->only(['post_article', 'update', 'destroy']);
+    }
+
+    // post article linked to news
+    public function post_article(Request $request, News $news){
+        $request->validate([
+            'content' => 'required|string',
+        ]);
+
+        $article = new Article();
+        $article->content = $request->content;
+        $article->news_id = $news->id;
+        $article->save();
+
+        return response()->json($article, 201);
+    }
 }
